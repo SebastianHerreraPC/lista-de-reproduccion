4698b862c10fa5126feeba7918eadfb2862117b9
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-01 23:36:54 -0600

    seleccion de elementos y botones

--- script/index.js
+++ script/index.js
@@ -1,2 +1,14 @@
 //seleccion de elemento
 let container = document.querySelector(".container ");
+let songsContainer = container.querySelector(".songs-container");
+let songs = songsContainer.querySelectorAll(".song");
+
+//variables de botones
+let addButton = container.querySelector(".form__submit-btn_action_add");
+let resetButton = container.querySelector(".form__submit-btn_action_reset");
+
+//boton reset apagado si no hay canciones
+
+if (songs.length === 0) {
+  resetButton.setAttribute("disabled", true);
+}
