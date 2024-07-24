c0a476ddaf393eba8deb8befbee5c59b7b120578
SebastianHerreraPC <sebehepe00@gmail.com>
2024-07-24 13:46:27 -0600

    arreglos formulatrio

--- script/index2.js
+++ script/index2.js
@@ -2,7 +2,8 @@ const songsContainer = document.querySelector(".songs-container");
 const addButton = document.querySelector(".input__btn_action_add");
 const artistInput = document.querySelector(".input__text_type_artist");
 const titleInput = document.querySelector(".input__text_type_title");
-const form = document.forms.add;
+const artist = form.elements.artist;
+const title = form.elements.title;
 
 function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector("#song-template").content;
@@ -12,15 +13,28 @@ function addSong(artistValue, titleValue) {
   songsContainer.append(songElement);
 }
 
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault(); // Prevenir el envío del formulario
+function setSubmitButtonState(isFormValid) {
+  if (isFormValid) {
+    addButton.removeAttribute("disabled");
+    addButton.classList.remove("input__btn_disabled");
+  } else {
+    addButton.setAttribute("disabled", true);
+    addButton.classList.add("input__btn_disabled");
+  }
+}
 
-  const artistValue = artistInput.value; // Obtener los valores de los inputs
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  const artistValue = artistInput.value;
   const titleValue = titleInput.value;
 
-  addSong(artistValue, titleValue); // Llamar a la función con los valores correctos
-  artistInput.value = ""; // Limpiar los campos del formulario
+  addSong(artistValue, titleValue);
+  artistInput.value = "";
   titleInput.value = "";
 });
 
-form.addEventListener;
+form.addEventListener("input", function (evt) {
+  const isValid = artist.value.length > 0 && title.value.length > 0;
+  console.log(evt);
+});
