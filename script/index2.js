a92eaa2097e38bd17aaa7dda6d2aab5010e56d6d
SebastianHerreraPC <sebehepe00@gmail.com>
2024-07-24 12:35:41 -0600

    valores de formulario

--- script/index2.js
+++ script/index2.js
@@ -9,16 +9,14 @@ const title = form.elements.title;
 function addSong(artistValue, titleValue) {
   const songTemplate = document.querySelector("#song-template").content;
   const songElement = songTemplate.cloneNode(true);
-
   songElement.querySelector(".song__artist").textContent = artistValue;
   songElement.querySelector(".song__title").textContent = titleValue;
-
   songsContainer.append(songElement);
 }
 
 form.addEventListener("submit", function (evt) {
-  addSong(artistInput.value, titleInput.value);
-  artistInput.value = "";
+  addSong(artistValue.value, titleValue.value);
   titleInput.value = "";
+  artistInput.value = "";
   evt.preventDefault();
 });
